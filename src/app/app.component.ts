5ce823367144e92c7065091661b0a80c4b77a8e8
AvishekBarua <avishek_barua@yahoo.com>
2017-04-17 12:30:34 +0300

    Photo-list component component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html',
+  moduleId: module.id,
+  templateUrl: 'app.component.html',
 })
 export class AppComponent  { }
